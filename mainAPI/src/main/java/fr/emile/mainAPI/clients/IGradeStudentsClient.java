5527b1993d145e7d97f21bd6a3fcaa744bcc994e
emile roche ubuntu <emile.roche@ynov.com>
2020-02-03 15:06:41 +0100

    fix conflict

--- mainAPI/src/main/java/fr/emile/mainAPI/clients/IGradeStudentsClient.java
+++ mainAPI/src/main/java/fr/emile/mainAPI/clients/IGradeStudentsClient.java
@@ -6,11 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
 @Component
-<<<<<<< HEAD
-@FeignClient(name = "UserTodosClient")
-=======
 @FeignClient(name = "GradeStudentsClient")
->>>>>>> master
 public interface IGradeStudentsClient {
     @RequestLine("GET /{id}") //execute l'appel
     Object getGradeStudents(@Param("id") Long id);
